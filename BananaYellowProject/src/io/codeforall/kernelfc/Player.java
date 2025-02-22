37d631cebd8b312005c10ac7baf62a0c1dd7a1aa
brunoAnastacio <brunoduarte.91300@gmail.com>
2025-02-22 18:07:08 +0000

    added player class + sout im a chicken in main

---
+++ BananaYellowProject/src/io/codeforall/kernelfc/Player.java
@@ -0,0 +1,5 @@
+package io.codeforall.kernelfc;
+
+public class Player {
+
+}
--- BananaYellowProject/src/io/codeforall/kernelfc/Playground.java
+++ BananaYellowProject/src/io/codeforall/kernelfc/Playground.java
@@ -3,5 +3,6 @@ package io.codeforall.kernelfc;
 public class Playground {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("im a chicken");
     }
 }
