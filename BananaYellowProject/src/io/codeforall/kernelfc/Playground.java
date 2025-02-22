9334f7057eb198414c038126c4a7cd97ed3341ae
Rafael Penacho <rafapenacho@gmail.com>
2025-02-22 18:26:44 +0000

    Fixed file issues

--- BananaYellowProject/src/io/codeforall/kernelfc/Playground.java
+++ BananaYellowProject/src/io/codeforall/kernelfc/Playground.java
@@ -4,5 +4,7 @@ public class Playground {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("im a chicken");
+
+        System.out.println("im also a chicken named rafael");
     }
 }
