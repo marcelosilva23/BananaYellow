11d5a6a196d6043f062c2ce10d150f4e202f4539
Rafael Penacho <rafapenacho@gmail.com>
2025-02-26 18:10:21 +0000

    added comments, removed StartingX from pipe

---
+++ BananaYellowProject/src/io/codeforall/kernelfc/Game.java
@@ -0,0 +1,18 @@
+/*
+package io.codeforall.kernelfc;
+
+public class Game {
+
+    private int player;
+    private int score;
+
+    private pipeFactory[];
+
+
+<<<<<<< HEAD
+    
+=======
+>>>>>>> 61b40f8 (Merge pull,moved pseudo)
+
+}
+*/
